2b48cb821e691f45849b30c497e2f0ea4a1a53a6
raflidaffa <112566735+raflidaffa@users.noreply.github.com>
2023-11-29 23:29:11 +0700

    Update Admin.java

--- PerpustakaanOrang/src/perpustakaanorang/Admin.java
+++ PerpustakaanOrang/src/perpustakaanorang/Admin.java
@@ -9,5 +9,35 @@ package perpustakaanorang;
  * @author ACER
  */
 public class Admin {
-    
+    private int idAdmin;
+    private String namaAdmin;
+
+    public Admin(int idAdmin, String namaAdmin) {
+        this.idAdmin = idAdmin;
+        this.namaAdmin = namaAdmin;
+    }
+
+    public void kelolaBuku() {
+        System.out.println("Admin \"" + namaAdmin + "\" menambahkan buku baru.");
+    }
+
+    public void kelolaAnggota() {
+        System.out.println("Admin \"" + namaAdmin + "\" mengelola anggota perpustakaan.");
+    }
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNamaAdmin() {
+        return namaAdmin;
+    }
+
+    public void setNamaAdmin(String namaAdmin) {
+        this.namaAdmin = namaAdmin;
+    }
 }
